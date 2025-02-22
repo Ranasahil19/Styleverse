3bebefe7bf3203f9f45a0b7e33a11662ad8b722f
Sujal-Polawala <sujalpolawala@gmail.com>
2025-02-22 19:35:52 +0530

    Add product and order list routes

--- admin/src/routes/AdminRoutes.js
+++ admin/src/routes/AdminRoutes.js
@@ -1,31 +1,31 @@
-import { lazy } from "react";
-import Loadable from "ui-component/Loadable";
-import MainLayout from "layout/MainLayout";
-import UserProfile from "component/Profile";
-import AccountSettings from "component/AccountSetting";
-import CategoriesList from "views/pages/admin/CategoriesPage";
+import { lazy } from 'react';
+import Loadable from 'ui-component/Loadable';
+import MainLayout from 'layout/MainLayout';
+import UserProfile from 'component/Profile';
+import AccountSettings from 'component/AccountSetting';
+import CategoriesList from 'views/pages/admin/CategoriesPage';
 
-const AdminDashboard = Loadable(lazy(() => import("views/pages/admin/AdminDashboard")));
-const UserList = Loadable(lazy(() => import("views/pages/admin/userList")));
-const SellerList = Loadable(lazy(() => import("views/pages/admin/SellerList")));
- const ProductList = Loadable(lazy(() => import("views/pages/admin/ProductList")));
-const OrderList = Loadable(lazy(() => import("views/pages/admin/OrderList")));
-const SamplePage = Loadable(lazy(() => import("views/sample-page")));
+const AdminDashboard = Loadable(lazy(() => import('views/pages/admin/AdminDashboard')));
+const UserList = Loadable(lazy(() => import('views/pages/admin/userList')));
+const SellerList = Loadable(lazy(() => import('views/pages/admin/SellerList')));
+const ProductList = Loadable(lazy(() => import('views/pages/admin/ProductList')));
+const OrderList = Loadable(lazy(() => import('views/pages/admin/OrderList')));
+const SamplePage = Loadable(lazy(() => import('views/sample-page')));
 
 const AdminRoutes = {
-    path : "/admin",
-    element: <MainLayout />,
-    children : [
-        { path:"dashboard" , element: <AdminDashboard />},
-        { path:"user-list" , element: <UserList />},
-        { path:"seller-list", element: <SellerList />},
-        { path:"categories-list", element: <CategoriesList />},
-        { path: "product-list", element: <ProductList /> },
-        { path: "order-list", element: <OrderList /> },
-        // Add more routes here...
-        { path: "sample-page", element: <SamplePage /> },
-        {path : "profile" , element: <UserProfile/>},
-        {path: "account-setting", element: <AccountSettings/>}
-    ]
-}
+  path: '/admin',
+  element: <MainLayout />,
+  children: [
+    { path: 'dashboard', element: <AdminDashboard /> },
+    { path: 'user-list', element: <UserList /> },
+    { path: 'seller-list', element: <SellerList /> },
+    { path: 'categories-list', element: <CategoriesList /> },
+    { path: 'product-list', element: <ProductList /> },
+    { path: 'order-list', element: <OrderList /> },
+    // Add more routes here...
+    { path: 'sample-page', element: <SamplePage /> },
+    { path: 'profile', element: <UserProfile /> },
+    { path: 'account-setting', element: <AccountSettings /> }
+  ]
+};
 export default AdminRoutes;
