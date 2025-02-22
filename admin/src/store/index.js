c2ca914dcc4fa78cd4cc73ebd8dfe8c14e68d139
Sujal-Polawala <sujalpolawala@gmail.com>
2025-02-22 19:36:30 +0530

    Add orderReducer in index

--- admin/src/store/index.js
+++ admin/src/store/index.js
@@ -1,28 +1,29 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import storage from "redux-persist/lib/storage";
-import { persistReducer, persistStore } from "redux-persist";
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 
-import customizationReducer from "./customizationReducer";
-import registrationReducer from "../features/registerSlice";
-import authReducer from "../features/authSlice";
-import productReducer from "../features/productSlice";
-import categoriesReducer from "features/categorySlice";
-import sellerReducer from "features/sellerSlice";
-import orderReducer from "../features/orderSlice";
-const getRootReducer = () => combineReducers({
-  customization: customizationReducer,
-  register: registrationReducer,
-  auth: authReducer,
-  products: productReducer,
-  categories: categoriesReducer,
-  seller : sellerReducer,
-  orders: orderReducer,
-});
+import customizationReducer from './customizationReducer';
+import registrationReducer from '../features/registerSlice';
+import authReducer from '../features/authSlice';
+import productReducer from '../features/productSlice';
+import categoriesReducer from 'features/categorySlice';
+import sellerReducer from 'features/sellerSlice';
+import orderReducer from '../features/orderSlice';
+const getRootReducer = () =>
+  combineReducers({
+    customization: customizationReducer,
+    register: registrationReducer,
+    auth: authReducer,
+    products: productReducer,
+    categories: categoriesReducer,
+    seller: sellerReducer,
+    orders: orderReducer
+  });
 
 const persistConfig = {
-  key: "auth",
+  key: 'auth',
   storage,
-  whitelist: ["auth"],
+  whitelist: ['auth']
 };
 
 const persistedReducer = persistReducer(persistConfig, getRootReducer());
@@ -31,8 +32,8 @@ const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
-    }),
+      serializableCheck: false
+    })
 });
 
 const persistor = persistStore(store);
