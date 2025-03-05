73741e6b7382c09ab2756720ec113349fa214e4c
Sahil <ranasahil.sr29@gmail.com>
2025-03-05 13:21:08 +0530

    mark as all read

--- backend/routes/notification/notificationRoutes.js
+++ backend/routes/notification/notificationRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getNotification, markAsRead, deleteNotification } = require('../../controllers/notifications/notificationController');
+const { getNotification, markAsRead, deleteNotification, markAsAllRead } = require('../../controllers/notifications/notificationController');
 const router = express.Router();
 
 router.get('/:sellerId' , getNotification)
@@ -7,5 +7,7 @@ router.get('/:sellerId' , getNotification)
 router.put('/mark-as-read/:notificationId', markAsRead)
 
 router.delete('/:notificationId', deleteNotification)
+
+router.put('/mark-as-all-read', markAsAllRead)
 
 module.exports = router;
